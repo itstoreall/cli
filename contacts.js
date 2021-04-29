4768084554bb4cfe6edc6d19aa147fb08b1a5781
Serhii Stanislav <serhiistanislav@gmail.com>
2021-04-29 11:48:40 +0300

    adds a switch to the index.js

--- contacts.js
+++ contacts.js
@@ -7,30 +7,60 @@ async function listContacts() {
     const contacts = await fs.readFile(contactsPath, 'utf-8');
     return await JSON.parse(contacts);
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+    return;
   }
 }
 
 async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const contact = contacts.find((contact) => {
-    return contact.id === contactId;
-  });
-
-  return contact;
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => {
+      return contact.id === contactId;
+    });
+    return contact;
+  } catch (error) {
+    console.log(error.message);
+    return;
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = JSON.stringify(
+      [...contacts, { id: Date.now(), name: name, email: email, phone: phone }],
+      null,
+      2
+    );
+    await fs.writeFile(contactsPath, newContact);
+    return contacts;
+    // console.log('44', contacts);
+  } catch (error) {
+    console.log(error.message);
+    return;
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+// addContact('Vasya', 'f@mail.net', '0678856849');
+// addContact();
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const removedContacts = contacts.filter((contact) => {
+      return contact.id !== contactId;
+    });
+    return fs.writeFile(contactsPath, JSON.stringify(removedContacts, null, 2));
+  } catch (error) {
+    console.log(error.message);
+    return;
+  }
 }
 
 const contactFn = { listContacts, getContactById, removeContact, addContact };
-
 export default contactFn;
+
 /* Example #1
 export const info = (msg) => {
   console.log(`Info: ${msg}`);
--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-import fs from 'fs';
-import contactFn from './contacts.js';
+// import fs from 'fs';
+import functions from './contacts.js';
+import { Command } from 'commander/esm.mjs';
 
-console.log(
-  contactFn.listContacts().then((data) =>
-    console.log(
-      'ind-fn1:',
-      data.map((d) => d.id)
-    )
-  )
-);
+const program = new Command();
 
-console.log(
-  contactFn.getContactById(7).then((data) => console.log('ind-fn2:', data.id))
-);
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      functions.listContacts().then((data) => console.log(data));
+      break;
+
+    case 'get':
+      functions.getContactById(id).then((data) => console.log(data));
+      break;
+
+    case 'add':
+      functions
+        .addContact(name, email, phone)
+        .then((data) => console.log(data));
+      break;
+
+    case 'remove':
+      functions.removeContact(id).then((data) => console.log(data));
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
+
+// console.log(
+//   contactFn.listContacts().then((data) =>
+//     console.log(
+//       'ind-fn1:',
+//       data.map((d) => d.id)
+//     )
+//   )
+// );
+
+// console.log(
+//   contactFn.getContactById(7).then((data) => console.log('ind-fn2:', data.id))
+// );
+
+// console.log('ind-removeContact', contactFn.removeContact(2));
+
+// =========================
 
 // contactFn.listContacts();
 // import path from 'path';
